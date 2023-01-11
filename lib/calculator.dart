f04eff9fa0e830905325b1098472c8be6030259c
bora <bora@artlab.ai>
2023-01-11 17:47:10 +0900

    feat: add 2 functions

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,7 +1,9 @@
 library calculator;
 
-/// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+  int plusOne(int value) => value + 1;
+
+  /// Returns [value] minus 1.
+  int minusOne(int value) => value - 1;
 }

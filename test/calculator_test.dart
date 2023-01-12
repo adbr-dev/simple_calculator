14769781c50431eccfd716a3b207dd497991393f
bora <adbr.dev@gmail.com>
2023-01-12 18:02:04 +0900

    feat: add test code

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -3,9 +3,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:calculator/calculator.dart';
 
 void main() {
-  test('adds one to input values', () {
+  test('plus one to input values', () {
     expect(Calculator.plusOne(2), 3);
     expect(Calculator.plusOne(-7), -6);
     expect(Calculator.plusOne(0), 1);
+  });
+
+  test('minus one to input values', () {
+    expect(Calculator.minusOne(2), 1);
+    expect(Calculator.minusOne(-7), -8);
+    expect(Calculator.minusOne(0), -1);
   });
 }
